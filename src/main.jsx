4dd6ac66c48984f74e3a160b89b8a95f48291dee
Vahid-developer <vahidbanakar19@gmail.com>
2026-09-21 12:09:23 +0330

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Header from './components/Header'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header />
+  </StrictMode>,
+)
